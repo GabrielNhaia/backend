c94e37635583a99f2db3b3fd23e52c09416b3754
Gabriel Nhaia <gabriel.nhaia@gmail.com>
2026-04-15 16:07:06 -0300

    feat:Implements an authentication controller with registration, login, user recovery, and logout.

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
+// routes/api.php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login',    [AuthController::class, 'login']);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth:api')->group(function () {
+    Route::get('/me',      [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
